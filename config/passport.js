43042595b7e2e5517737a753f9e358538f0d2de0
AneeshP99 <aneeshpandit2021.comp@mmcoe.edu.in>
2024-09-14 19:29:52 +0530

    updated config

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,4 @@
+//const passport = require('passport');
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const config = require('./config');
 const { User } = require('../models');
@@ -9,18 +10,22 @@ const jwtOptions = {
 
 const jwtVerify = async (payload, done) => {
   try {
+    console.log('JWT payload:', payload); // Log payload for debugging
     const user = await User.findById(payload._id);
     if (!user) {
+      console.log('User not found'); // Log if user is not found
       return done(null, false);
     }
     done(null, user);
   } catch (error) {
+    console.log('JWT verify error:', error); // Log errors
     done(error, false);
   }
 };
 
 const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
 
-module.exports = {
-  jwtStrategy,
-};
+// Initialize Passport and use the JWT strategy
+//passport.use(jwtStrategy);
+
+module.exports = {jwtStrategy}; // Export passport for use in the main file
--- config/roles.js
+++ config/roles.js
@@ -1,13 +1,12 @@
 const allRoles = {
-    employee: ['read', 'create', 'update'],
-    admin: ['create', 'read', 'update', 'delete'],
-  };
-  
-  const roles = Object.keys(allRoles);
-  const roleRights = new Map(Object.entries(allRoles));
-  
-  module.exports = {
-    roles,
-    roleRights,
-  };
-  
+  employee: ['read', 'create', 'update'],
+  admin: ['create', 'read', 'update', 'delete'],
+};
+
+const roles = Object.keys(allRoles);
+const roleRights = new Map(Object.entries(allRoles));
+
+module.exports = {
+  roles,
+  roleRights,
+};
