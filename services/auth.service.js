79f09770f65b7ce3867dfc685254acafea81260d
AneeshP99 <aneeshpandit2021.comp@mmcoe.edu.in>
2024-10-02 16:00:56 +0530

    fixed services

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,42 +1,26 @@
 const httpStatus = require('http-status');
 const tokenService = require('./token.service');
-const userService = require('./user.service');
-const Token = require('../models/token.model');
 const { tokenTypes } = require('../config/tokens');
+const User = require('../models/users.model');
+const bcrypt = require('bcrypt');
 
 const loginUserWithEmailAndPassword = async (email, password) => {
-  const user = await userService.getUserByEmail(email);
-  if (!user || !(await user.password === password)) {
-    throw new Error(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
-  }
-  return user;
-};
-
-const logout = async (refreshToken) => {
-  const refreshTokenDoc = await Token.findOne({ token: refreshToken, type: tokenTypes.REFRESH, blacklisted: false });
-  if (!refreshTokenDoc) {
-    throw new Error(httpStatus.NOT_FOUND, 'Not found');
-  }
-  await refreshTokenDoc.remove();
-};
-
-const refreshAuth = async (refreshToken) => {
-  try {
-    const refreshTokenDoc = await tokenService.verifyToken(refreshToken, tokenTypes.REFRESH);
-    const user = await userService.getUserById(refreshTokenDoc.user);
+  const user = await User.findOne({ email });
     if (!user) {
-      throw new Error();
+        throw new Error(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
     }
-    await refreshTokenDoc.remove();
-    return tokenService.generateAuthTokens(user);
-  } catch (error) {
-    throw new Error(httpStatus.UNAUTHORIZED, 'Please authenticate');
-  }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
+    }
+
+    return user;
 };
+
+
 
 
 module.exports = {
   loginUserWithEmailAndPassword,
-  logout,
-  refreshAuth,
 };
